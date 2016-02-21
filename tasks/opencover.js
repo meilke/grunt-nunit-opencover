935ab3d82f6f823f20b7884427055ec0a781c1de
Christian Meilke <christian.meilke@googlemail.com>
2016-02-21 14:09:48 +0100

    More testsand improved code

--- tasks/opencover.js
+++ tasks/opencover.js
@@ -1,24 +1,27 @@
 exports.wrapCommand = function(command, options) {
+
   var pathToOpencover = options.coverPath;
   var wrappedArgs = [];
+  
+  var addOption = function (propertyName, propertyNameCommand, defaultValue) {
+    if (options[propertyName] && options[propertyName] !== '') {
+      wrappedArgs.push('' + propertyNameCommand + ':"' + options[propertyName] + '"');
+      return;
+    }
+
+    if (defaultValue) {
+      wrappedArgs.push('' + propertyNameCommand + ':' + defaultValue);
+    }
+  };
+
   wrappedArgs.push('-target:"' + command.path + '"');
   wrappedArgs.push('-targetargs:"' + command.args.join(' ') + '"');
   wrappedArgs.push('-register:user');
   wrappedArgs.push('-returntargetcode');
   
-  var coverReportFilePath = '_CodeCoverageResult.xml';
-  if (options.coverReportFilePath && options.coverReportFilePath !== '') {
-    coverReportFilePath = '"' + options.coverReportFilePath + '"';
-  }
-  wrappedArgs.push('-output:' + coverReportFilePath);
-
-  if (options.coverFilter && options.coverFilter !== '') {
-    wrappedArgs.push('-filter:"' + options.coverFilter + '"');
-  }
-
-  if (options.coverExcludeAttributeFilter && options.coverExcludeAttributeFilter !== '') {
-    wrappedArgs.push('-excludebyattribute:"' + options.coverExcludeAttributeFilter + '"');
-  }
+  addOption('coverReportFilePath', '-output', '_CodeCoverageResult.xml');
+  addOption('coverFilter', '-filter');
+  addOption('coverExcludeAttributeFilter', '-excludebyattribute');
 
   return {
     path: pathToOpencover,
--- test/opencover.spec.js
+++ test/opencover.spec.js
@@ -3,23 +3,71 @@ var expect = require('expect.js'),
 
 describe('opencover', function() {
 
-  it('should wrap the NUnit command', function() {
+  var command = {
+    path: 'to/nunit',
+    args: ['first_arg', 'second_arg']
+  };
 
-    var command = {
-      path: 'to/nunit',
-      args: ['first_arg', 'second_arg']
-    };
+  var options = {
+    coverPath: 'to/cover',
+    coverExcludeAttributeFilter: 'ceaFilter',
+    coverFilter: 'cFilter'
+  };
 
-    var options = { coverPath: 'to/cover' };
+  var result = opencover.wrapCommand(command, options);
 
-    var result = opencover.wrapCommand(command, options);
+  it('should wrap the OpenCover command', function() {
     expect(result.path).to.be('to/cover');
-    expect(result.args).to.contain('-target:"to/nunit"');
-    expect(result.args).to.contain('-targetargs:"first_arg second_arg"');
-    expect(result.args).to.contain('-output:_CodeCoverageResult.xml');
-    expect(result.args).to.contain('-register:user');
-    expect(result.args).to.contain('-returntargetcode');
+  });
 
+  it('should wrap the NUnit command', function() {
+    expect(result.args).to.contain('-target:"to/nunit"');
+  });
+
+  it('should wrap the NUnit arguments', function() {    
+    expect(result.args).to.contain('-targetargs:"first_arg second_arg"');
+  });
+
+  it('should set the default XML output path', function() {
+    expect(result.args).to.contain('-output:_CodeCoverageResult.xml');
+  });
+
+  it('should set the XML output path', function() {
+    var specialResult = opencover.wrapCommand(command, {
+      coverPath: 'to/cover',
+      coverReportFilePath: 'some/path'
+    });
+    expect(specialResult.args).to.contain('-output:"some/path"');
+  });
+
+  it('should use the register:user option', function() {
+    expect(result.args).to.contain('-register:user');
+  });
+
+  it('should use the returntargetcode option', function() {
+    expect(result.args).to.contain('-returntargetcode');
+  });
+
+  it('should set an exclude attribute filter', function() {
+    expect(result.args).to.contain('-excludebyattribute:"ceaFilter"');
+  });
+
+  it('should not set an exclude attribute filter', function() {
+    var specialResult = opencover.wrapCommand(command, {
+      coverPath: 'to/cover'
+    });
+    expect(specialResult.args).to.not.contain('-excludebyattribute');
+  });
+
+  it('should set the filter option', function() {
+    expect(result.args).to.contain('-filter:"cFilter"');
+  });
+
+  it('should not set the filter option', function() {
+    var specialResult = opencover.wrapCommand(command, {
+      coverPath: 'to/cover'
+    });
+    expect(specialResult.args).to.not.contain('-filter');
   });
 
 });
